d85c1943da26069b64aa71b0bbb2eaf7f5e8ddcc
Evan Schaffer <ess2129@shake.cm.cluster>
2021-01-05 15:14:00 -0500

    shell script for regModel on moto

---
+++ scripts/run_regression_model.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH -A axs
+#SBATCH -J s0
+#SBATCH -t 11:59:00
+#SBATCH -c 12
+#SBATCH -o r0.out -e r0.err
+#SBATCH --mem-per-cpu=8gb
+
+expID=0
+module load anaconda/3-5.3.1
+source activate flygenvectors
+python run_regression_model_moto.py $expID
+
+
+
+
+
+
+
+
+
+
+
