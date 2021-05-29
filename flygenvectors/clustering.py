07b7a1938c3d6fba72c4589ba25ca6e2d4e5d307
schafferEvan <schafferEvan@users.noreply.github.com>
2021-05-29 15:49:01 -0400

    added get_linkage for dendrogram

--- flygenvectors/clustering.py
+++ flygenvectors/clustering.py
@@ -31,8 +31,8 @@ from matplotlib.colors import LinearSegmentedColormap
 import data as dataUtils
 import regression_model as model
 import plotting
-import flygenvectors.ssmutils as utils
-import flygenvectors.utils as futils
+# import flygenvectors.ssmutils as utils
+# import flygenvectors.utils as futils
 from sklearn.linear_model import ElasticNet
 
 import seaborn as sns
@@ -344,6 +344,25 @@ class flyg_clust_obj:
         plt.tight_layout() 
         # plt.savefig(fig_dirs['clustfig_folder']+expt_id+'_clust_dist_score_hist.pdf',transparent=True, bbox_inches='tight')
         # plt.show()
+
+
+    def get_linkage(self):
+        # Create linkage matrix and then plot the dendrogram
+        # create the counts of samples under each node
+        model = self.cluster
+        counts = np.zeros(model.children_.shape[0])
+        n_samples = len(model.labels_)
+        for i, merge in enumerate(model.children_):
+            current_count = 0
+            for child_idx in merge:
+                if child_idx < n_samples:
+                    current_count += 1  # leaf node
+                else:
+                    current_count += counts[child_idx - n_samples]
+            counts[i] = current_count
+        linkage_matrix = np.column_stack([model.children_, model.distances_,
+                                          counts]).astype(float)
+        return linkage_matrix
                 
 
 class mvn_obj:
